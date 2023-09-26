5d3eb9b3ce9361a17bf1af6977674e66ff523ff5
octopusthu <octopusthu@gmail.com>
2023-09-26 15:05:28 +0800

    Add tests

--- playground/time_test.go
+++ playground/time_test.go
@@ -14,6 +14,14 @@ func TestParseTime(t *testing.T) {
 	fmt.Println("parsedTime: ", parsedTime)
 }
 
+func TestParseTimeLayout(t *testing.T) {
+	parsedTime, err := time.Parse("2006-01-02 00:00:00", "2023-09-19 00:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println("parsedTime: ", parsedTime)
+}
+
 func TestParseTimeInLocation(t *testing.T) {
 	localLocation, _ := time.LoadLocation("Local")
 	parsedTime, err := time.ParseInLocation(time.DateTime, "2023-06-08 10:10:10", localLocation)
@@ -26,4 +34,44 @@ func TestParseTimeInLocation(t *testing.T) {
 func TestTimeFormat(t *testing.T) {
 	now := time.Now()
 	fmt.Println(now.Format("20060102"))
+}
+
+func TestTimeFormatLayout(t *testing.T) {
+	now := time.Now()
+	fmt.Println(now.Format("2006-01-02 01:01:01"))
+}
+
+func TestAddMinusOneMonthToJanuary(t *testing.T) {
+	january, _ := time.Parse(time.DateTime, "2023-01-01 01:01:01")
+	fmt.Println(lastMonth(january).Format(time.DateTime))
+}
+
+func lastMonth(t time.Time) time.Time {
+	return t.AddDate(0, -1, 0)
+}
+
+func TestLastMonth(t *testing.T) {
+	var m time.Time
+	m, _ = time.Parse(time.DateTime, "2023-05-01 00:00:00")
+	fmt.Printf("lastMonth(%v): %v\n", m, lastMonth(m))
+	m, _ = time.Parse(time.DateTime, "2023-05-31 00:00:00")
+	fmt.Printf("lastMonth(%v): %v\n", m, lastMonth(m))
+	m, _ = time.Parse(time.DateTime, "2023-03-01 00:00:00")
+	fmt.Printf("lastMonth(%v): %v\n", m, lastMonth(m))
+}
+
+func lastDayOfMonth(t time.Time) time.Time {
+	firstDayOfNextMonth := time.Date(t.Year(), t.Month()+1, 1, 0, 0, 0, 0, t.Location())
+	lastDayOfThisMonth := firstDayOfNextMonth.AddDate(0, 0, -1)
+	return lastDayOfThisMonth
+}
+
+func TestLastDayOfMonth(t *testing.T) {
+	var m time.Time
+	m, _ = time.Parse(time.DateTime, "2023-05-01 00:00:00")
+	fmt.Printf("lastDayOfMonth(%v): %v\n", m, lastDayOfMonth(m))
+	m, _ = time.Parse(time.DateTime, "2023-05-31 00:00:00")
+	fmt.Printf("lastDayOfMonth(%v): %v\n", m, lastDayOfMonth(m))
+	m, _ = time.Parse(time.DateTime, "2023-03-01 00:00:00")
+	fmt.Printf("lastDayOfMonth(%v): %v\n", m, lastDayOfMonth(m))
 }
