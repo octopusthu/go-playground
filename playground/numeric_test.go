5643dd2150f6791a1d2bfd07f568bea4350e3c43
octopusthu <octopusthu@gmail.com>
2023-10-01 18:28:20 +0800

    Add TestUintAsResultOfNegativeDifference

--- playground/numeric_test.go
+++ playground/numeric_test.go
@@ -9,3 +9,12 @@ import (
 func TestParseUint(t *testing.T) {
 	fmt.Println(strconv.ParseUint("20230101", 10, 32))
 }
+
+func TestUintAsResultOfNegativeDifference(t *testing.T) {
+	var uintResult uint64
+	var minuend, subtrahend uint64
+	minuend = 1
+	subtrahend = 2
+	uintResult = minuend - subtrahend
+	fmt.Printf("%d - %d = (uint64)%d\n", minuend, subtrahend, uintResult)
+}
