060c6be867b5b0c2200816d5bb8dfaacc510fd59
octopusthu <octopusthu@gmail.com>
2024-04-11 11:21:55 +0800

    Add TestPanicAfterDefer

--- playground/defer_test.go
+++ playground/defer_test.go
@@ -1,17 +1,25 @@
 package playground
 
 import (
-	"fmt"
-	"testing"
+    "fmt"
+    "testing"
 )
 
 func TestSetStructFieldAfterDefer(t *testing.T) {
-	type s struct {
-		a string
-	}
-	str := s{a: "initial value"}
-	defer func() {
-		fmt.Println("value evaluated by defer is: ", str.a)
-	}()
-	str.a = "modified value"
+    type s struct {
+        a string
+    }
+    str := s{a: "initial value"}
+    defer func() {
+        fmt.Println("value evaluated by defer is: ", str.a)
+    }()
+    str.a = "modified value"
+}
+
+func TestPanicAfterDefer(t *testing.T) {
+    defer func() {
+        fmt.Println("defer statement executed")
+    }()
+    fmt.Println("before panic")
+    panic("panicked")
 }
